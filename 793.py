1e74343afc4551d43ab79c1ce8a635e65793d7f3
DerrickChanCS <derrick.chan@icloud.com>
2018-10-10 16:33:40 -0700

    Create 793.py

---
+++ 793.py
@@ -0,0 +1,46 @@
+"""
+Let f(x) be the number of zeroes at the end of x!. (Recall that x! = 1 * 2 * 3 * ... * x, and by convention, 0! = 1.)
+
+For example, f(3) = 0 because 3! = 6 has no zeroes at the end, while f(11) = 2 because 11! = 39916800 has 2 zeroes at the end. Given K, find how many non-negative integers x have the property that f(x) = K.
+
+Example 1:
+Input: K = 0
+Output: 5
+Explanation: 0!, 1!, 2!, 3!, and 4! end with K = 0 zeroes.
+
+Example 2:
+Input: K = 5
+Output: 0
+Explanation: There is no x such that x! ends in K = 5 zeroes.
+Note:
+
+K will be an integer in the range [0, 10^9].
+"""
+
+class Solution(object):
+    def preimageSizeFZF(self, K):
+        """
+        :type K: int
+        :rtype: int
+        """
+        low = 0
+        high = 5 * (K+1)
+        
+        while low <= high:
+            mid = low + (high - low)/2
+            zeros = self.numZeros(mid)
+            if zeros < K:
+                low = mid + 1
+            elif zeros > K:
+                high = mid - 1
+            else:
+                return 5
+        return 0
+        
+    def numZeros(self, x):
+        res = 0
+        while x > 0:
+            import math
+            res += math.floor(x/5)
+            x = math.floor(x/5)
+        return res
