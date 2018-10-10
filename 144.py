b764a602f583faded3cd41c31e488cb192cec846
DerrickChanCS <swampyrules@gmail.com>
2018-10-10 15:38:09 -0700

    Create 144.py

---
+++ 144.py
@@ -0,0 +1,44 @@
+"""
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result = []
+        stack = []
+        while root is not None:
+            result.append(root.val)
+            if root.right is not None:
+                stack.append(root.right)
+            if root.left is not None:
+                stack.append(root.left)
+            if stack:
+                root = stack.pop()
+            else:
+                root = None
+        return result
+            
