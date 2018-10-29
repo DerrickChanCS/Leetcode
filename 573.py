d8b6a915b438593a8bf757be5e692860eda8a925
DerrickChanCS <derrick.chan@icloud.com>
2018-10-29 15:26:01 -0700

    Create 573.py

---
+++ 573.py
@@ -0,0 +1,40 @@
+"""
+There's a tree, a squirrel, and several nuts. Positions are represented by the cells in a 2D grid. Your goal is to find the minimal distance for the squirrel to collect all the nuts and put them under the tree one by one. The squirrel can only take at most one nut at one time and can move in four directions - up, down, left and right, to the adjacent cell. The distance is represented by the number of moves.
+Example 1:
+
+Input: 
+Height : 5
+Width : 7
+Tree position : [2,2]
+Squirrel : [4,4]
+Nuts : [[3,0], [2,5]]
+Output: 12
+Explanation:
+​​​​​
+Note:
+
+All given positions won't overlap.
+The squirrel can take at most one nut at one time.
+The given positions of nuts have no order.
+Height and width are positive integers. 3 <= height * width <= 10,000.
+The given positions contain at least one nut, only one tree and one squirrel.
+"""
+
+class Solution(object):
+    def minDistance(self, height, width, tree, squirrel, nuts):
+        """
+        :type height: int
+        :type width: int
+        :type tree: List[int]
+        :type squirrel: List[int]
+        :type nuts: List[List[int]]
+        :rtype: int
+        """
+        sum = 0
+        maxDist = float('-inf')
+        for i in nuts:
+            x, y = i[1], i[0]
+            dist = abs(y - tree[0]) + abs(x - tree[1])
+            sum += 2 * dist
+            maxDist = max(maxDist, dist - abs(y - squirrel[0]) - abs(x - squirrel[1]))
+        return sum - maxDist
