2a5cec787aa008dd1075856ba719bd52dd3d7fbf
DerrickChanCS <derrick.chan@icloud.com>
2018-11-11 16:46:45 -0800

    Create 088.py

---
+++ 088.py
@@ -0,0 +1,33 @@
+"""
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+"""
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        j = n - 1
+        i = m - 1
+        insert = n + m - 1
+        while j >= 0 and i >= 0:            
+            if nums1[i] >= nums2[j]:
+                nums1[insert] = nums1[i]
+                i -= 1
+            else:
+                nums1[insert] = nums2[j]
+                j -= 1
+            insert -= 1
+        while j >= 0:
+            nums1[insert] = nums2[j]
+            j -= 1
+            insert -= 1
