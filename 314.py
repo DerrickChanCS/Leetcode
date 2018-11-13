52716b8683accfff283d44e49cb618c652d44fb5
DerrickChanCS <derrick.chan@icloud.com>
2018-11-13 12:50:58 -0800

    Create 314.py

---
+++ 314.py
@@ -0,0 +1,108 @@
+"""
+Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
+
+If two nodes are in the same row and column, the order should be from left to right.
+
+Examples 1:
+
+Input: [3,9,20,null,null,15,7]
+
+   3
+  /\
+ /  \
+ 9  20
+    /\
+   /  \
+  15   7 
+
+Output:
+
+[
+  [9],
+  [3,15],
+  [20],
+  [7]
+]
+Examples 2:
+
+Input: [3,9,8,4,0,1,7]
+
+     3
+    /\
+   /  \
+   9   8
+  /\  /\
+ /  \/  \
+ 4  01   7 
+
+Output:
+
+[
+  [4],
+  [9],
+  [3,0,1],
+  [8],
+  [7]
+]
+Examples 3:
+
+Input: [3,9,8,4,0,1,7,null,null,null,2,5] (0's right child is 2 and 1's left child is 5)
+
+     3
+    /\
+   /  \
+   9   8
+  /\  /\
+ /  \/  \
+ 4  01   7
+    /\
+   /  \
+   5   2
+
+Output:
+
+[
+  [4],
+  [9,5],
+  [3,0,1],
+  [8,2],
+  [7]
+]
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def verticalOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        q = collections.deque()
+        #(node, degree)
+        #degree is how left or right a node is
+        # -degree is more left
+        ans = []
+        seen = collections.defaultdict(list)
+        
+        q.append((root, 0))
+        
+        while q:
+            node, degree = q.popleft()
+            if node:
+                seen[degree].append(node.val)
+                q.append((node.left, degree-1))
+                q.append((node.right, degree+1))
+                
+        for key in sorted(seen.iterkeys()):
+            ans.append(seen[key])
+        return ans
+            
+            
+            
